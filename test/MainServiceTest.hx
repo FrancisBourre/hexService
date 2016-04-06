a2fdb7b218ea9acf21d2514e2565e355aa5350c7
aliokan <laurent@geturl.net>
2016-04-06 10:58:44 +0200

    add loaderInfo in TraceNotifier constructor

--- test/MainServiceTest.hx
+++ test/MainServiceTest.hx
@@ -5,6 +5,7 @@ import hex.unittest.runner.ExMachinaUnitCore;
 import hex.unittest.runner.TestRunner;
 
 #if flash
+import flash.Lib;
 import hex.unittest.notifier.TraceNotifier;
 #else
 import hex.unittest.notifier.ConsoleNotifier;
@@ -21,7 +22,7 @@ class MainServiceTest
 		var emu = new ExMachinaUnitCore();
         
 		#if flash
-		emu.addListener( new TraceNotifier( false ) );
+		emu.addListener( new TraceNotifier( Lib.current.loaderInfo ) );
 		#else
 		emu.addListener( new ConsoleNotifier( false ) );
 		#end
