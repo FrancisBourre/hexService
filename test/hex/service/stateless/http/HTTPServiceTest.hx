5e9df51f8f8d68cf64967090daf7721fe80cadf0
Laurent Deketelaere <laurent.deketelaere@doclerholding.com>
2016-08-25 16:22:30 +0200

    fix MockParser definition due to DoclerLabs/hexCore#9 changes

--- test/hex/service/stateless/http/HTTPServiceTest.hx
+++ test/hex/service/stateless/http/HTTPServiceTest.hx
@@ -387,7 +387,7 @@ private class MockParser
 		
 	}
 
-	public function parse( serializedContent : Dynamic ) : Dynamic 
+	public function parse( serializedContent : Dynamic, target : Dynamic = null ) : Dynamic 
 	{
 		return serializedContent + 1;
 	}
