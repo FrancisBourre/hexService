3991652cc5fd92736c88b10d4c556335db151133
Francis Bourre <francis_bourre@me.com>
2017-12-01 10:08:42 +0100

    ServiceMonitor removed

--- test/hex/service/ServiceServiceSuite.hx
+++ test/hex/service/ServiceServiceSuite.hx
@@ -1,7 +1,6 @@
 package hex.service;
 
 import hex.service.AbstractServiceTest;
-import hex.service.monitor.MVCMonitorSuite;
 import hex.service.stateful.MVCStatefulServiceSuite;
 import hex.service.stateless.MVCStatelessServiceSuite;
 
@@ -12,5 +11,5 @@ import hex.service.stateless.MVCStatelessServiceSuite;
 class ServiceServiceSuite
 {
 	@Suite( "Service" )
-    public var list : Array<Class<Dynamic>> = [ MVCMonitorSuite, MVCStatelessServiceSuite, MVCStatefulServiceSuite, AbstractServiceTest, ServiceConfigurationTest, ServiceURLConfigurationTest ];
+    public var list : Array<Class<Dynamic>> = [ MVCStatelessServiceSuite, MVCStatefulServiceSuite, AbstractServiceTest, ServiceConfigurationTest, ServiceURLConfigurationTest ];
 }
